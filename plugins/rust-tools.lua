b634e1ebcbb5bc56920a2dc4543c804a765deb61
Viterkim <vkc@connectedcars.dk>
2023-05-08 14:09:08 +0200

    undo breaking of rust-tools

--- plugins/rust-tools.lua
+++ plugins/rust-tools.lua
@@ -14,17 +14,19 @@ return {
         liblldb_path = vim.env.HOME .. "/.local/share/nvim/mason/packages/codelldb/extension/lldb/lib/liblldb.so"
       end
 
+      -- Feature stuff, but this breaks go to def, and ref
+      -- server = {
+      --   require("astronvim.utils.lsp").config "rust_analyzer",
+      --   settings = {
+      --     ['rust-analyzer'] = {
+      --       cargo = {
+      --         features = "all"
+      --       }
+      --     }
+      --   }
+      -- },
       return {
-        server = {
-          require("astronvim.utils.lsp").config "rust_analyzer",
-          settings = {
-            ['rust-analyzer'] = {
-              cargo = {
-                features = "all"
-              }
-            }
-          }
-        },
+        server = require("astronvim.utils.lsp").config "rust_analyzer",
         dap = {
           adapter = require("rust-tools.dap").get_codelldb_adapter(codelldb_path, liblldb_path),
         },
