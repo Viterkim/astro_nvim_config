39d8d7def8cebdb7eef515c182581305f910eede
Viterkim <vkc@connectedcars.dk>
2023-11-27 15:25:02 +0100

    ya

--- plugins/mason.lua
+++ plugins/mason.lua
@@ -13,7 +13,7 @@ return {
     "jay-babu/mason-null-ls.nvim",
     -- overrides `require("mason-null-ls").setup(...)`
     opts = {
-      ensure_installed = { "eslint_d", "fixjson", "rustfmt@v1.5.1" },
+      ensure_installed = { "eslint_d", "fixjson" },
     },
   },
   {
